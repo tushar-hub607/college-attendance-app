20536edd54b280bccde4154688fb7ae6b66b91e0
tushar-hub607 <raghuvanshi7tushar@gmail.com>
2026-08-08 13:15:36 +0530

    Bsckend ready with mongoDB atlas

--- attendance/lib/services/api_service.dart
+++ attendance/lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import '../models/user_model.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://localhost:5000/api';
+  static const String baseUrl = 'https://college-attendance-app-opnc.onrender.com';
 
   static Future<User?> login(String email, String password) async {
     try {
