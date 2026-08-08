c80d0e6330fdf41a25e1365cc7998a7b9527ba72
tushar-hub607 <raghuvanshi7tushar@gmail.com>
2026-08-08 15:31:10 +0530

    update mongo uri in server.js

--- backend/server.js
+++ backend/server.js
@@ -226,5 +226,5 @@ app.get('/api/reports/:userId/:role', async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
